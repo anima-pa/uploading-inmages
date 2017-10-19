f77cd78891cd393111dca437d71d20b81ded4304
anima-pa <anima1111@outlook.com>
2017-10-19 15:36:06 +0530

    initsl

---
+++ galleryyy/galleryyy.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("galleryyy.Droid.MainApplication, galleryyy.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md51cf7104e732e95d49d410860888b6ff9.MainApplication.class, md51cf7104e732e95d49d410860888b6ff9.MainApplication.__md_methods);
+		
+	}
+}
---
+++ galleryyy/galleryyy/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace galleryyy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void CaptureImage_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", " No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                SaveToAlbum = true,
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            PicturePic.Source = ImageSource.FromStream(() => file.GetStream());
+
+        }
+
+        private async void PickFromGallery_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("photo not supported", "Picking photo from memory not supported", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+            PicturePic.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+    }
+}
